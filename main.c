3a7dbbf14c8e63b24c4eb0a11635ce5d9803ddc0
chepeniv <jose.olmos@atlasschool.com>
2024-07-31 22:08:17 -0500

    [passing] open and read .m file

--- main.c
+++ main.c
@@ -2,13 +2,29 @@
 
 int main(int argc, char **argv)
 {
+	FILE *mfile;
+	char *file_line = NULL;
+	size_t read_bytes;
+	char *open_mode = "r";
+
 	if (argc != 2)
 	{
 		dprintf(STDERR_FILENO, "USAGE: monty <file>\n" );
 		return (EXIT_FAILURE);
 	}
 
-	printf("filename given: %s\n", argv[1]);
+	mfile = fopen(argv[1], open_mode);
+	if (mfile == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Cant't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+
+	while (getline(&file_line, &read_bytes, mfile) > -1)
+		printf("%s", file_line);
+
+	free(file_line);
+	fclose(mfile);
 
 	return (EXIT_SUCCESS);
 }
